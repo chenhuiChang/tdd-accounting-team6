8e4c7edd96b06b1ab4d22b547b86271487a37c3d
chenhuiChang <j4demacia@gmail.com>
2019-02-19 01:29:25 +0800

    refactor

--- src/Accounting.php
+++ src/Accounting.php
@@ -44,31 +44,31 @@ class Accounting
         if ($period->isSameMonth()) {
             foreach ($this->budgetRepo->getAll() as $budget) {
                 if ($budget->start()->isSameMonth($period->start())) {
-                    $totalAmount += $budget->dailyAmount() * $period->days();
+                    $overlappingStart = $period->start()->day;
+                    $overlappingEnd = $period->end()->day;
+                    $overlappingDays = $overlappingEnd - $overlappingStart + 1;
+                    $totalAmount += $budget->dailyAmount() * $overlappingDays;
                 }
             }
             return $totalAmount;
         } else {
             foreach ($this->budgetRepo->getAll() as $budget) {
                 if ($budget->start()->isAfter($period->end()) || $budget->end()->isBefore($period->start())) {
-                    $totalAmount += 0;
-                } else if ($budget->start()->isBefore($period->start())) {
-                    $overlappingDays = $budget->days() - $period->start()->day + 1;
-                    $totalAmount += $budget->dailyAmount() * $overlappingDays;
-//                    $totalAmount += $budget->dailyAmount() * ($overlappingEnd->diffInDays($overlappingStart));
-//                    $overlappingDays = $budget->end()->diffInDays($period->start()) + 1;
-//                    $totalAmount += $budget->dailyAmount() * $overlappingDays;
-                } else if ($budget->end()->isAfter($period->end())) {
-                    $overlappingStart = $budget->start()->day;
-                    $overlappingEnd = $period->end()->day;
-                    $totalAmount += $budget->dailyAmount() * ($overlappingEnd - $overlappingStart + 1);
-                } else if ($budget->start()->isAfter($period->start()) && $budget->end()->isBefore($period->end())) {
-                    $overlappingStart = $budget->start();
-                    $overlappingEnd = $budget->end();
-                    $totalAmount += $budget->dailyAmount() * ($overlappingEnd->diffInDays($overlappingStart) + 1);
+                    $overlappingDays = 0;
                 } else {
-                    $totalAmount += 0;
+                    if ($budget->start()->isBefore($period->start())) {
+                        $overlappingStart = $period->start()->day;
+                        $overlappingEnd = $budget->end()->day;
+                    } else if ($budget->end()->isAfter($period->end())) {
+                        $overlappingStart = $budget->start()->day;
+                        $overlappingEnd = $period->end()->day;
+                    } else if ($budget->start()->isAfter($period->start()) && $budget->end()->isBefore($period->end())) {
+                        $overlappingStart = $budget->start()->day;
+                        $overlappingEnd = $budget->end()->day;
+                    }
+                    $overlappingDays = $overlappingEnd - $overlappingStart + 1;
                 }
+                $totalAmount += $budget->dailyAmount() * $overlappingDays;
             }
         }
         return $totalAmount;
